2cd9b81404bfdd2c30e5e40f048687b5b3371df6
Milton Chithenga <88409286+MarcuzAC@users.noreply.github.com>
2026-01-29 11:34:41 +0200

    Searc field

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1465,8 +1465,8 @@ const Products = () => {
             }
             
             .search-section, .input-container {
-              min-width: 100% !important;
-              max-width: 100% !important;
+              min-width: 80% !important;
+              max-width: 80% !important;
             }
             
             .select-style {
